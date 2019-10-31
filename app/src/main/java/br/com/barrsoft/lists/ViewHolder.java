3a93b717cef979836d188ca5ef1dd0c4941df731
Reginaldo Barreto <reginaldo.barreto@gmail.com>
2019-10-31 19:01:55 -0300

    adicionado a classe ViewHolder

---
+++ app/src/main/java/br/com/barrsoft/lists/ViewHolder.java
@@ -0,0 +1,34 @@
+package br.com.barrsoft.lists;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.nio.ByteBuffer;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    @BindView(R.id.imageView)
+    ImageView imageView;
+    @BindView(R.id.textTitle)
+    TextView title;
+    @BindView(R.id.textSubTitle)
+    TextView subTitle;
+    @BindView(R.id.button)
+    AppCompatButton button;
+
+
+
+    public ViewHolder(@NonNull View itemView) {
+        super(itemView);
+        ButterKnife.bind(this,itemView);
+    }
+}
